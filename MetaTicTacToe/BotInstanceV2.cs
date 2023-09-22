0bb5a0577ceabdb5de0bc02a3035a3cbd2459ae4
Kurt <k.sap@journaway.com>
2023-09-22 12:23:51 +0200

    added structur to botv2

--- MetaTicTacToe/BotInstanceV2.cs
+++ MetaTicTacToe/BotInstanceV2.cs
@@ -1,4 +1,6 @@
-﻿namespace MetaTicTacToe;
+﻿using System.Runtime.InteropServices.JavaScript;
+
+namespace MetaTicTacToe;
 
 public class BotInstanceV2 : IBotInstance
 {
@@ -7,8 +9,41 @@ public class BotInstanceV2 : IBotInstance
         
     }
 
-    public void DoMove(Game game)
+    public int[] DoMove(Game game)
     {
-        throw new NotImplementedException();
+        var resultWinField = WinField(game);
+        if (resultWinField.Length < 1)
+        {
+            return Array.Empty<int>();
+        }
+        
+        var resultBlockOpponent = BlockOpponent(game);
+        if (resultBlockOpponent.Length <1)
+        {
+            return Array.Empty<int>();
+        }
+
+        return ChooseField(game);
+    }
+
+    private int[] BlockOpponent(Game game)
+    {
+        //do stuff
+
+        return Array.Empty<int>();
+    }
+
+    private int[] WinField(Game game)
+    {
+        //do stuff 
+
+        return Array.Empty<int>();
+    }
+
+    private int[] ChooseField(Game game)
+    {
+        //do stuff 
+
+        return Array.Empty<int>();
     }
 }
