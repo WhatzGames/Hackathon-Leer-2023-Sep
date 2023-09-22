8a23099786414a4e6454e32feed0e22fbc16e2bb
Kevin Plenter <k.plenter@journaway.com>
2023-09-22 17:21:11 +0200

    Fix layout

--- meta-tic-tac-to-fe/app/layout.tsx
+++ meta-tic-tac-to-fe/app/layout.tsx
@@ -1,25 +1,25 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Poppins, Josefin_Sans } from 'next/font/google'
+import './globals.css';
+import type {Metadata} from 'next';
+import {Josefin_Sans} from 'next/font/google';
 import {cn} from '@/lib/cn';
 
-const font = Josefin_Sans({ subsets: ['latin'], weight: ['400', '600'] })
+const font = Josefin_Sans({subsets: ['latin'], weight: ['400', '600']});
 
 export const metadata: Metadata = {
   title: 'JAckathon - Meta Tic-Tac-Toe',
   description: 'Proudly presented by journaway',
-}
+};
 
 export default function RootLayout({
-  children,
-}: {
+                                     children,
+                                   }: {
   children: React.ReactNode
 }) {
   return (
     <html lang="de">
-      <body className={cn(font.className, 'w-screen h-screen flex justify-center items-center')}>
-        {children}
-      </body>
+    <body className={cn(font.className, 'w-screen h-screen flex justify-center items-center')}>
+    {children}
+    </body>
     </html>
-  )
+  );
 }
