7bbeed5cbdb333c2fea834668b01a31c67124fcf
Kevin Plenter <k.plenter@journaway.com>
2023-09-22 15:03:31 +0200

    We can play!

--- meta-tic-tac-to-fe/app/game-store.ts
+++ meta-tic-tac-to-fe/app/game-store.ts
@@ -1,43 +1,93 @@
 import {create} from 'zustand';
 
-type Game = string[][];
+type Game = Board[];
+type Board = (Player | null)[];
 type Player = 'X' | 'O';
 
 type GameStore = {
   player: Player;
+  activeBoard: number | null;
+  finishedBoards: (Player | null)[];
   game: Game;
   setField: (board: number, field: number) => void;
 }
 
-const setField = (game: Game, player: Player, board: number, field: number): Game => {
-  game[board][field] = player;
-  return game;
-};
+const checkBoard = (board: Board): Player | null => {
+  const combinations = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+  ];
+
+  for (const combination of combinations) {
+    if (!board[combination[0]] || !board[combination[1]] || !board[combination[2]]) {
+      continue;
+    }
+
+    if (board[combination[0]] === board[combination[1]] && board[combination[1]] === board[combination[2]]) {
+      return board[combination[0]];
+    }
+  }
+
+  return null;
+}
 
 export const useGameStore = create<GameStore>((set, get) => ({
   player: 'X',
+  activeBoard: null,
+  finishedBoards: [null, null, null, null, null, null, null, null, null],
   game: [
-    ['', '', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '', ''],
+    [null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null],
   ],
   setField(board: number, field: number) {
-    const game = get().game;
-
-    if (game[board][field] !== '') {
-      return; // value set - not allowed!
+    // board not active - not allowed
+    let activeBoard = get().activeBoard;
+    if (activeBoard !== null && board !== activeBoard) {
+      return;
     }
+
+    // Board finished - not allowed
+    const finishedBoards = get().finishedBoards;
+    if (finishedBoards[board] !== null) {
+      return;
+    }
+
+    // Field set - not allowed
+    const game = get().game;
+    if (game[board][field] != null) {
+      return;
+    }
+
+    // Set field to player
+    game[board][field] = get().player;
+
+    const winner = checkBoard(game[board]);
+    if (winner) {
+      finishedBoards[board] = winner;
+    }
+
+    // Set new active to last field, otherwise allow random choice
+    activeBoard = finishedBoards[field] != null ? null : field;
 
     set((state) => ({
       ...state,
       player: state.player === 'X' ? 'O' : 'X',
-      game: setField(state.game, state.player, board, field)
+      activeBoard,
+      finishedBoards,
+      game
     }));
   }
 }));
--- meta-tic-tac-to-fe/app/page.tsx
+++ meta-tic-tac-to-fe/app/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 
 import {useGameStore} from '@/app/game-store';
+import {cn} from '@/lib/cn';
 
 export default function Game() {
   const {game} = useGameStore();
 
   return (
-    <div className={'grid grid-cols-3 grid-rows-3 gap-2'}>
+    <div className={'grid grid-cols-3 grid-rows-3 gap-3'}>
       {game.map((board, index) => <Board key={index} boardNo={index} board={board}/>)}
     </div>
   );
@@ -14,13 +15,28 @@ export default function Game() {
 
 type BoardProps = {
   boardNo: number;
-  board: string[];
+  board: (string | null)[];
 }
 
 const Board = ({boardNo, board}: BoardProps) => {
+  const activeBoard = useGameStore(state => state.activeBoard);
+  const finishedBoards = useGameStore(state => state.finishedBoards);
+  const winner = finishedBoards[boardNo];
+
   return (
-    <div className={'grid grid-cols-3 grid-rows-3'}>
-      {board.map((field, index) => <Field key={index} board={boardNo} field={index}>{field}</Field>)}
+    <div className={'relative'}>
+      {winner &&
+        <div className={'text-6xl text-red-500 absolute bg-slate-700/60 w-full h-full flex justify-center items-center'}>
+          {winner}
+        </div>
+      }
+      <div className={cn(
+        'grid grid-cols-3 grid-rows-3 gap-1',
+        activeBoard === boardNo && 'border-2 border-red-600',
+        winner && 'border-red-950'
+      )}>
+        {board.map((field, index) => <Field key={index} board={boardNo} field={index}>{field}</Field>)}
+      </div>
     </div>
   );
 };
@@ -38,7 +54,7 @@ const Field = ({board, field, width = 60, height = 60, children}: React.PropsWit
   const handleClick = () => setField(board, field);
 
   return (
-    <button className={'bg-red-300 border border-black'}
+    <button className={'border border-cyan-400 hover:bg-cyan-300 hover:animate-pulse'}
             onClick={handleClick}
             style={{width, height}}>
       {children}
