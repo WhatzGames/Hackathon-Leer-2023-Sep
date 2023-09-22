a41084096f957b5089d804da18f1586ec6fb75d9
Kevin Plenter <k.plenter@journaway.com>
2023-09-22 11:50:39 +0200

    Use pnpm - because it is great!

--- meta-tic-tac-to-fe/app/page.tsx
+++ meta-tic-tac-to-fe/app/page.tsx
@@ -1,5 +1,7 @@
 export default function Game() {
   return (
-    <div></div>
+    <div className={'grid grid-cols-3 grid-rows-3'}>
+      <div className={'bg-red border border-black'} style={{width: 24, height: 24}}></div>
+    </div>
   )
 }
