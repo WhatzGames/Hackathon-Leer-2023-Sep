9041eb68ce3c2731c7337c769aeb62abb67de6e0
Kevin Plenter <k.plenter@journaway.com>
2023-09-22 19:43:11 +0200

    Read files from path

--- meta-tic-tac-to-fe/app/api/history/route.ts
+++ meta-tic-tac-to-fe/app/api/history/route.ts
@@ -1,13 +1,35 @@
 import {NextResponse} from 'next/server';
-import * as fs from 'node:fs';
+import {readdirSync, readFileSync, statSync} from 'fs';
 import path from 'path';
+import {PlayedGame} from '@/app/types';
 
-export function GET() {
+export async function GET() {
   const dirRelativeToPublicDir = 'history';
   const dir = path.resolve('./public', dirRelativeToPublicDir);
 
-  const fileNames = fs.readdirSync(dir);
-  const files = fileNames.map(name => path.join('/', dirRelativeToPublicDir, name));
+  const files = readdirSync(dir);
+  const filePaths = files.map(name => {
+    const filePath = path.resolve('./public', dirRelativeToPublicDir, name);
 
-  return NextResponse.json(files);
+    return {
+      filePath,
+      time: statSync(filePath).mtime.getTime()
+    };
+  })
+    .sort((a, b) => a.time - b.time)
+    .map(file => file.filePath);
+
+  const games: PlayedGame[] = [];
+  filePaths.forEach((filePath) => {
+    let data = readFileSync(filePath, 'utf-8');
+
+    try {
+      const game = JSON.parse(data.trim());
+      games.push(game);
+    } catch (e) {
+      console.error('could not parse json', e);
+    }
+  });
+
+  return NextResponse.json(games);
 }
