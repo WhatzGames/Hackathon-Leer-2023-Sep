d84fa011fe743ab35407251d2128e79620a1b14b
Kevin Plenter <k.plenter@journaway.com>
2023-09-23 14:01:24 +0200

    Add random name

--- meta-tic-tac-to-fe/lib/random-name.ts
+++ meta-tic-tac-to-fe/lib/random-name.ts
@@ -1,7 +1,7 @@
 const names = ['Jar Jar Binks', 'Amy Schumer', 'UnityEngine', 'Moq', 'Elon Musk', 'Martin Shkreli', 'Andrew Wilson',
-  'X Æ A-12', 'Phallicator', 'Nutnibbler'];
+  'X Æ A-12', 'Phallicator', 'Nutnibbler', 'Bowser', 'C64', 'N64', 'Sega Megadrive', 'Sega Dreamcast'];
 
 export function randomName() {
-  const index = Math.round(Math.random() * names.length);
+  const index = Math.round(Math.random() * (names.length - 1));
   return names[index];
 }
